4569c56d4d5ea44332f5180208fa5c96120473aa
rawan <rawanesmandar0@gmail.com>
2024-09-30 13:11:25 +0300

    modified

---
+++ Modules/AuthAuthorize/database/seeders/AuthAuthorizeDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\AuthAuthorize\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class AuthAuthorizeDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
---
+++ Modules/AuthAuthorize/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('authauthorize::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>Module: {!! config('authauthorize.name') !!}</p>
+@endsection
